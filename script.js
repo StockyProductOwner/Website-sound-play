2d4d62e5bdf142563e8aa3937c604aeda4ba02da
StockyProductOwner <ruthdabooth@gmail.com>
2024-10-06 11:03:49 +0100

    Updated script.js to handle audio playback errors

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ document.addEventListener('keydown', function(event) {
   if (event.code === 'Space') {
     var audio = document.getElementById('myAudio');
     if (audio.paused) {
-      audio.play();
+      audio.play().catch(error => {
+        console.error('Error playing audio:', error);
+      }); 
     } else {
       audio.pause();
     }
