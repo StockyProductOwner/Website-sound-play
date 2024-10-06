9a8a5800f48750a7adacfa150e66cc882ad3246b
StockyProductOwner <ruthdabooth@gmail.com>
2024-10-06 11:08:04 +0100

    Simplified script.js to use ternary operator for play/pause.

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
 document.addEventListener('keydown', function(event) {
   if (event.code === 'Space') {
     var audio = document.getElementById('myAudio');
-    if (audio.paused) {
-      audio.play().catch(error => {
-        console.error('Error playing audio:', error);
-      }); 
-    } else {
-      audio.pause();
-    }
+    audio.paused ? audio.play() : audio.pause();
   }
 });
